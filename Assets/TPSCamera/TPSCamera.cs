50796890f0c792c1b2f90cdc1a3ae7dc8310baca
Nimanji <terataka.ggl@gmail.com>
2018-06-26 09:00:31 +0900

    TPSカメラもどきの実装

---
+++ Assets/TPSCamera/TPSCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class TPSCamera : MonoBehaviour
+{
+	/// <value>視点基準とするGameObject。Inspectorから指定。</value>
+	public GameObject tps_target;
+
+	double deg_diff;
+
+	void Awake ()
+	{
+		// 視点対象を基準にしてカメラの位置を決定する
+		transform.position = tps_target.transform.position + new Vector3(1.0f, 5.0f, -1.0f);
+		// カメラの位置と視点基準の角度を割り出す
+		deg_diff = Mathf.Atan2(transform.position.z-tps_target.transform.position.z, transform.position.x-tps_target.transform.position.x) * (180/Mathf.PI);
+	}
+
+	void Start ()
+	{
+		Observable.EveryUpdate().Subscribe(_ => calcTpsPosition());
+	}
+
+	void calcTpsPosition ()
+	{
+		// 調整したdegからオブジェクトの座標を修正する
+		float rad = (float)(deg_diff - tps_target.transform.localEulerAngles.y * (Mathf.PI/180));
+		transform.position = tps_target.transform.position + (new Vector3(Mathf.Cos(rad)*1.0f, 0.0f, Mathf.Sin(rad)*1.0f) + new Vector3(0.0f, 1.4f, 0.0f));
+		// カメラの方向を視点基準と同期させる
+		transform.rotation = tps_target.transform.rotation;
+	}
+}
---
+++ Assets/TPSCamera/WASDMoved.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class WASDMoved : MonoBehaviour
+{
+	const float MOVE_SPEED = 0.1f;
+	const float ROTATE_SPEED = 1.0f;
+
+	void Start ()
+	{
+		// キャラクターの移動
+		Observable.EveryUpdate().Where(_ => Input.GetKey(KeyCode.W)).Subscribe(_ => movedByKeyCode(KeyCode.W));
+		Observable.EveryUpdate().Where(_ => Input.GetKey(KeyCode.A)).Subscribe(_ => movedByKeyCode(KeyCode.A));
+		Observable.EveryUpdate().Where(_ => Input.GetKey(KeyCode.S)).Subscribe(_ => movedByKeyCode(KeyCode.S));
+		Observable.EveryUpdate().Where(_ => Input.GetKey(KeyCode.D)).Subscribe(_ => movedByKeyCode(KeyCode.D));
+		// キャラクターの向きを変える
+		Observable.EveryUpdate().Where(_ => Input.GetKey(KeyCode.LeftArrow)).Subscribe(_ => rotateByKeyCode(KeyCode.LeftArrow));
+		Observable.EveryUpdate().Where(_ => Input.GetKey(KeyCode.RightArrow)).Subscribe(_ => rotateByKeyCode(KeyCode.RightArrow));
+	}
+
+	/// <summary>
+	/// 入力のあった方向にこのGameObjectを移動させる
+	/// </summary>
+	void movedByKeyCode(KeyCode code)
+	{
+		float mv_x = 0.0f;
+		float mv_z = 0.0f;
+
+		switch (code) {
+			case KeyCode.W:
+				mv_z = MOVE_SPEED;
+				break;
+			case KeyCode.A:
+				mv_x = MOVE_SPEED * -1;
+				break;
+			case KeyCode.S:
+				mv_z = MOVE_SPEED * -1;
+				break;
+			case KeyCode.D:
+				mv_x = MOVE_SPEED;
+				break;
+		}
+
+		Vector3 move_vec = new Vector3(mv_x, 0.0f, mv_z);
+
+		this.transform.position += move_vec;
+	}
+
+	/// <summary>
+	/// 入力のあった方向にこのGameObjectの角度を変える
+	/// </summary>
+	void rotateByKeyCode(KeyCode code)
+	{
+		switch (code) {
+			case KeyCode.LeftArrow:
+				transform.Rotate(new Vector3(0.0f, ROTATE_SPEED * -1, 0.0f));
+				break;
+			case KeyCode.RightArrow:
+				transform.Rotate(new Vector3(0.0f, ROTATE_SPEED, 0.0f));
+				break;
+		}
+	}
+}
